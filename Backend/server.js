3d30c709d3fa37b357ef1993c5302f33a625cd0e
are yaar <tanmaybhanushali2003@gmail.com>
2026-06-19 02:19:20 +0530

    add vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,8 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
     
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
